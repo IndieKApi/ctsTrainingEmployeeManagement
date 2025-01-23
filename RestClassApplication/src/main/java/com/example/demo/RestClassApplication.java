180f744f0f9cb6e63a6f001c05ddd84555092269
kapil hedau <kapil.hedau.official@gmail.com>
2025-01-23 11:17:15 +0530

    First push

---
+++ RestClassApplication/src/main/java/com/example/demo/RestClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClassApplication.class, args);
+	}
+
+}
